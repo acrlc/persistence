d068ccb02382f107f8284f55bc2defbd9618c618
William Luke <williamluke@me.com>
2023-12-29 10:37:39 -0600

    add serializable conversion

--- Sources/Conversion/CodeConversion.swift
+++ Sources/Conversion/CodeConversion.swift
@@ -21,3 +21,28 @@ public extension UserDefaultsKey where Value: AutoCodable {
 
 public protocol CodableDefaultsKey: UserDefaultsKey
  where Value: AutoCodable, Conversion == CodeConversion<Value> {}
+
+// MARK: Serializable
+public struct SerialConversion<Value: AutoSerializable>: ValueConversion {
+ public typealias Data = [String: Any]
+ public static func decode(data: Data) -> Value {
+  do {
+   return try Value.decoder.decode(Value.self, from: Value.deserialize(data))
+  }
+  catch { fatalError(error.message) }
+ }
+ 
+ public static func encode(value: Value) -> Data {
+  do { return try Value.serialize(Value.encoder.encode(value)) }
+  catch { fatalError(error.message) }
+ }
+}
+
+public extension UserDefaultsKey where Value: AutoSerializable {
+ typealias Conversion = SerialConversion<Value>
+}
+
+public protocol SerialDefaultsKey: UserDefaultsKey
+where Value: AutoSerializable, Conversion == SerialConversion<Value> {}
+
+
